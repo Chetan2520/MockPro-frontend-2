f496288a2ba2b0805d5371b66b69624ed8213528
Chetan Manker <148444931+Chetan2520@users.noreply.github.com>
2025-04-23 11:43:52 +0530

    Update vapi.js

--- src/vapi.js
+++ src/vapi.js
@@ -4,7 +4,7 @@ import Vapi from '@vapi-ai/web';
 let vapi = null;
 
 export const initVapi = async () => {
-  const res = await fetch('http://localhost:5000/api/vapi/token'); // adjust URL as needed
+  const res = await fetch('https://mockpro-backend.onrender.com/api/vapi/token'); // adjust URL as needed
   const data = await res.json();
   vapi = new Vapi(data.token);
   return vapi;
