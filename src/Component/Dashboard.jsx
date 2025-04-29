38bccdc43a5657b1a14132cd1df898bb7fb26d84
Chetan Manker <chetanmanker67@gmail.com>
2025-04-29 11:51:17 +0530

    .

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -49,7 +49,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://m-ock-pro-backend.vercel.app/api/database/items');
+        const response = await fetch('http://localhost:5000/api/database/items');
         const data = await response.json();
         setItem(data);
         setError(null);
